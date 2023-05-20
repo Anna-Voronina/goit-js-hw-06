d85fb74fe9761533caf721bf3f769b624fbe6622
Anna-Voronina <anna.vs2512@gmail.com>
2023-05-20 14:14:55 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const loginFormEl = document.querySelector(".login-form");
 
+const onLoginFormSubmit = (event) => {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(formData);
+  event.currentTarget.reset();
+  return formData;
+};
+
+loginFormEl.addEventListener("submit", onLoginFormSubmit);
