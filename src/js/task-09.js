3fd1e43e7aa049909ff586d361b27d56c158bf2e
Anna-Voronina <anna.vs2512@gmail.com>
2023-05-20 16:23:34 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,15 @@
+const changeColorBtnEl = document.querySelector(".change-color");
+const colorTextEl = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const onChangeColorBtnClick = (event) => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorTextEl.textContent = document.body.style.backgroundColor;
+};
+
+changeColorBtnEl.addEventListener("click", onChangeColorBtnClick);
